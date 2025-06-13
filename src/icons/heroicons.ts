ba20b63ceab737f1670dbea826e0cb3cb500b9c9
build-africa-eng <afrcanfuture@gmail.com>
2025-06-14 00:28:58 +0300

    Create heroicons.ts

---
+++ src/icons/heroicons.ts
@@ -0,0 +1,8 @@
+// src/icons/heroicons.ts
+import * as Heroicons from '@heroicons/react/24/outline';
+
+export function getHeroIcon(name: string): React.ElementType {
+  const toPascalCase = (str: string) =>
+    str.replace(/(^\w|-\w)/g, (g) => g.replace('-', '').toUpperCase());
+  return Heroicons[toPascalCase(name) as keyof typeof Heroicons] || Heroicons.QuestionMarkCircleIcon;
+}
