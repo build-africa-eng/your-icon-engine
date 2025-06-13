ad454bb4386954e42ab0e9a8e4b5fa8637d25339
build-africa-eng <afrcanfuture@gmail.com>
2025-06-14 01:02:19 +0300

    Update lucide.ts

--- src/icons/lucide.ts
+++ src/icons/lucide.ts
@@ -1,6 +1,5 @@
 // src/icons/lucide.ts
-import * as Lucide from 'lucide-react';
-
+const Lucide = await import('lucide-react');
 export function getLucideIcon(name: string): React.ElementType {
   const toPascalCase = (str: string) =>
     str.replace(/(^\w|-\w)/g, (g) => g.replace('-', '').toUpperCase());
