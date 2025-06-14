481422ef045c03d7b7f7752e8f869ca6176307b8
build-africa-eng <afrcanfuture@gmail.com>
2025-06-14 10:17:27 +0300

    Delete src/icons/tabler.ts

--- src/icons/tabler.ts
+++
@@ -1,8 +0,0 @@
-// src/icons/tabler.ts
-import * as Tabler from '@tabler/icons-react';
-
-export function getTablerIcon(name: string): React.ElementType {
-  const toPascalCase = (str: string) =>
-    str.replace(/(^\w|-\w)/g, (g) => g.replace('-', '').toUpperCase());
-  return Tabler[`Icon${toPascalCase(name)}` as keyof typeof Tabler] || Tabler.IconHelp;
-}
