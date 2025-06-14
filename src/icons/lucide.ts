77b0ad194f17aa24ac87259dfa4e15bb91466985
build-africa-eng <afrcanfuture@gmail.com>
2025-06-14 10:17:07 +0300

    Delete src/icons/lucide.ts

--- src/icons/lucide.ts
+++
@@ -1,8 +0,0 @@
-// src/icons/lucide.ts
-import * as Lucide from 'lucide-react';
-
-export function getLucideIcon(name: string): React.ElementType {
-  const toPascalCase = (str: string) =>
-    str.replace(/(^\w|-\w)/g, (g) => g.replace('-', '').toUpperCase());
-  return Lucide[toPascalCase(name) as keyof typeof Lucide] || Lucide.HelpCircle;
-}
