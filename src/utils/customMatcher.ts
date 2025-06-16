822818cc98802f9677660ca99277ccd9e79908ac
build-africa-eng <afrcanfuture@gmail.com>
2025-06-16 21:44:49 +0300

    Update customMatcher.ts

--- src/utils/customMatcher.ts
+++ src/utils/customMatcher.ts
@@ -1,9 +1,7 @@
-// src/utils/customMatcher.ts
 import { UtilityRule } from './types';
 
-// Custom rules for project-specific cases
 export const customUtilityIconMap: UtilityRule[] = [
-  { match: /^play-action$/, icon: 'playerPlay', source: 'tabler' },
+  { match: /^play-action$/, icon: 'player-play', source: 'tabler' },
   { match: /^world-map$/, icon: 'world', source: 'tabler' },
   { match: /^clock-time$/, icon: 'clock', source: 'tabler' },
 ];
