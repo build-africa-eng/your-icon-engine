e801b5a672783e36f4242bc2f1cdfe181bcbdb06
build-africa-eng <afrcanfuture@gmail.com>
2025-06-16 21:50:15 +0300

    Update IconRenderer.tsx

--- src/icons/IconRenderer.tsx
+++ src/icons/IconRenderer.tsx
@@ -1,46 +1,15 @@
 import React from 'react';
-import * as Lucide from 'lucide-react';
-import * as Heroicons from '@heroicons/react/24/outline';
-import * as Tabler from '@tabler/icons-react';
+import { iconAdapters, lucideAdapter } from './adapters';
 import { IconSource } from '@utils/types';
 
-const pascalCaseCache = new Map<string, string>();
-function toPascalCase(str: string): string {
-  if (pascalCaseCache.has(str)) return pascalCaseCache.get(str)!;
-  const camelCase = str.replace(/[-_]([a-z])/g, g => g[1].toUpperCase());
-  const pascal = camelCase.charAt(0).toUpperCase() + camelCase.slice(1);
-  pascalCaseCache.set(str, pascal);
-  return pascal;
+interface IconRendererProps {
+  icon: string;
+  source: IconSource;
+  className?: string;
 }
 
-export interface IconLibraryAdapter {
-  name: IconSource;
-  getIcon: (name: string) => React.ElementType;
+export function IconRenderer({ icon, source, className = 'w-5 h-5' }: IconRendererProps) {
+  const adapter = iconAdapters.get(source) || lucideAdapter;
+  const IconComponent = adapter.getIcon(icon);
+  return <IconComponent className={className} />;
 }
-
-export const lucideAdapter: IconLibraryAdapter = {
-  name: 'lucide',
-  getIcon: (name) => Lucide[toPascalCase(name) as keyof typeof Lucide] || Lucide.HelpCircle,
-};
-
-export const heroiconsAdapter: IconLibraryAdapter = {
-  name: 'heroicons',
-  getIcon: (name) => {
-    const iconName = `${toPascalCase(name)}Icon`;
-    return Heroicons[iconName as keyof typeof Heroicons] || Heroicons.QuestionMarkCircleIcon;
-  },
-};
-
-export const tablerAdapter: IconLibraryAdapter = {
-  name: 'tabler',
-  getIcon: (name) => {
-    const iconName = `Icon${toPascalCase(name)}`;
-    return Tabler[iconName as keyof typeof Tabler] || Tabler.IconHelp;
-  },
-};
-
-export const iconAdapters = new Map<IconSource, IconLibraryAdapter>([
-  [lucideAdapter.name, lucideAdapter],
-  [heroiconsAdapter.name, heroiconsAdapter],
-  [tablerAdapter.name, tablerAdapter],
-]);
