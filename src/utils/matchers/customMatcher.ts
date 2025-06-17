4974178961ef28a9762dbc4309352614d094005c
build-africa-eng <afrcanfuture@gmail.com>
2025-06-17 14:27:47 +0300

    Delete src/utils/matchers/customMatcher.ts

--- src/utils/matchers/customMatcher.ts
+++
@@ -1,7 +0,0 @@
-import { UtilityRule } from '../types';
-
-export const customUtilityIconMap: UtilityRule[] = [
-  { match: /^play-action$/, icon: 'player-play', source: 'tabler' },
-  { match: /^world-map$/, icon: 'world', source: 'tabler' },
-  { match: /^clock-time$/, icon: 'clock', source: 'tabler' },
-];
