e1d4382e4d3bbe86a4824ebab49f220aaffbe0f0
build-africa-eng <afrcanfuture@gmail.com>
2025-06-17 08:52:04 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-
+// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
-// This is the final line needed to fix the build.
-import './icons/library';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
