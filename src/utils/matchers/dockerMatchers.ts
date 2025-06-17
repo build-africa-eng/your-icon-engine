3ac9a154b577ff28a3d03b0bb6fbcaf397546b18
build-africa-eng <afrcanfuture@gmail.com>
2025-06-17 20:42:07 +0300

    Update dockerMatchers.ts

--- src/utils/matchers/dockerMatchers.ts
+++ src/utils/matchers/dockerMatchers.ts
@@ -2,10 +2,10 @@
 import { UtilityRule } from '../types';
 
 export const dockerMatchers: UtilityRule[] = [
-  { match: /^nginx$/, icon: 'brandnginx', source: 'tabler' },
-  { match: /^redis$/, icon: 'brandredis', source: 'tabler' },
-  { match: /^postgres/, icon: 'brandpostgresql', source: 'tabler' },
+  { match: /^nginx$/, icon: 'Brand-Nginx', source: 'tabler' },
+  { match: /^redis$/, icon: 'Brand-Redis', source: 'tabler' },
+  { match: /^postgres/, icon: 'Brand-Postgresql', source: 'tabler' },
   { match: /^mysql$/, icon: 'brandmysql', source: 'tabler' },
   { match: /^node$/, icon: 'BrandNodejs', source: 'tabler' },
-  { match: /^docker$/, icon: 'brandbocker', source: 'tabler' },
+  { match: /^docker$/, icon: 'Brand-Bocker', source: 'tabler' },
 ];
