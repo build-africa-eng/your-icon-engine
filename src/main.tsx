5e0e837d7857b98e0fff5d954e88cfe59160db92
build-africa-eng <afrcanfuture@gmail.com>
2025-06-17 08:36:29 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-// src/main.tsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
+// This is the final line needed to fix the build.
 import './icons/library';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
