aa447a76b95001054fe35c01b1266a604d40ec5f
build-africa-eng <afrcanfuture@gmail.com>
2025-06-17 23:29:33 +0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// src/index.ts
+export { IconRenderer } from './components/IconRenderer';
+export { allUtilityIconMap } from './matchers/allUtilityIconMap';
+export { iconEngine } from './core/iconEngine';
+export type { UtilityRule, IconSource, IconName } from './types';
