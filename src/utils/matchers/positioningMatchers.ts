aaaffa0422965116ab9e0dd6ede3daf411c2770d
build-africa-eng <afrcanfuture@gmail.com>
2025-06-17 15:20:14 +0300

    Update positioningMatchers.ts

--- src/utils/matchers/positioningMatchers.ts
+++ src/utils/matchers/positioningMatchers.ts
@@ -3,5 +3,6 @@ import { UtilityRule } from '../types';
 export const positioningMatchers: UtilityRule[] = [
   { match: /^absolute$/, icon: 'arrow-big-right-lines', source: 'tabler' },
   { match: /^relative$/, icon: 'arrow-big-left-lines', source: 'tabler' },
-  { match: /^z-/, icon: 'stack-2', source: 'tabler' }
+  { match: /^z-/, icon: 
+'stack-2', source: 'tabler' }
 ];
