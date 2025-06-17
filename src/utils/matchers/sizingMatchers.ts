ea7e10d5cae0894a09ace22a32de3af24e10a51a
build-africa-eng <afrcanfuture@gmail.com>
2025-06-17 17:19:04 +0300

    Update sizingMatchers.ts

--- src/utils/matchers/sizingMatchers.ts
+++ src/utils/matchers/sizingMatchers.ts
@@ -1,6 +1,12 @@
+// src/matchers/sizingMatchers.ts
 import { UtilityRule } from '../types';
 
 export const sizingMatchers: UtilityRule[] = [
-  { match: /^w-/, icon: 'arrow-autofit-width', source: 'tabler' },
-  { match: /^h-/, icon: 'arrow-autofit-height', source: 'tabler' },
+  { match: /^w-/, icon: 'ArrowsHorizontal', source: 'tabler' }, // width
+  { match: /^h-/, icon: 'ArrowsVertical', source: 'tabler' },   // height
+  { match: /^min-w-/, icon: 'ArrowBarToRight', source: 'tabler' },
+  { match: /^min-h-/, icon: 'ArrowBarToDown', source: 'tabler' },
+  { match: /^max-w-/, icon: 'ArrowBarRight', source: 'tabler' },
+  { match: /^max-h-/, icon: 'ArrowBarDown', source: 'tabler' },
+  { match: /^aspect-/, icon: 'AspectRatio', source: 'tabler' },
 ];
