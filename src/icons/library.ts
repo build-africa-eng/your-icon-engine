3b868b1c9fa91d09ffb66d93eb728c002eb7f8c8
build-africa-eng <afrcanfuture@gmail.com>
2025-06-17 16:35:13 +0300

    Update library.ts

--- src/icons/library.ts
+++ src/icons/library.ts
@@ -2,13 +2,13 @@
 import * as Lucide from 'lucide-react';
 import * as Heroicons from '@heroicons/react/24/outline';
 import * as Tabler from '@tabler/icons-react';
-import *as Phosphor from @phosphor-icons/react;
-
-import { IconLibrary } from '@utils/types';
+import * as Phosphor from '@phosphor-icons/react';
+import * as Iconoir from 'iconoir-react';
 
 export const AllIconLibraries: Record<IconSource, IconLibrary> = {
   lucide: Lucide,
   heroicons: Heroicons,
   tabler: Tabler,
   phosphor: Phosphor,
+  iconoir: Iconoir,
 };
